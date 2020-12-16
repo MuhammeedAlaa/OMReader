dcfe07da83d2e7a9f7a27586de1794285b417dd4
hossamalaa69 <hossamalaa69.ha@gmail.com>
2020-12-16 16:51:54 +0200

    Add Gaussian Filter

---
+++ commonfunctions.py
@@ -0,0 +1,47 @@
+
+
+import skimage.io as io
+import matplotlib.pyplot as plt
+import numpy as np
+from skimage.exposure import histogram
+from matplotlib.pyplot import bar
+from skimage.color import rgb2gray,rgb2hsv
+
+# Convolution:
+from scipy.signal import convolve2d
+from scipy import fftpack
+import math
+
+from skimage.util import random_noise
+from skimage.filters import median, gaussian
+from skimage.feature import canny
+
+# Show the figures / plots inside the notebook
+def show_images(images,titles=None):
+    #This function is used to show image(s) with titles by sending an array of images and an array of associated titles.
+    # images[0] will be drawn with the title titles[0] if exists
+    # You aren't required to understand this function, use it as-is.
+    n_ims = len(images)
+    if titles is None: titles = ['(%d)' % i for i in range(1,n_ims + 1)]
+    fig = plt.figure()
+    n = 1
+    for image,title in zip(images,titles):
+        a = fig.add_subplot(1,n_ims,n)
+        if image.ndim == 2: 
+            plt.gray()
+        max = 1
+        if image.dtype == "uint8":    
+            max = 255
+        plt.imshow(image, vmin = 0, vmax = max)
+        a.set_title(title)
+        n += 1
+    fig.set_size_inches(np.array(fig.get_size_inches()) * n_ims)
+    plt.show() 
+
+
+def showHist(img):
+    # An "interface" to matplotlib.axes.Axes.hist() method
+    plt.figure()
+    imgHist = histogram(img, nbins=256)
+    
+    bar(imgHist[1].astype(np.uint8), imgHist[0], width=0.8, align='center')
---
+++ main.py
@@ -0,0 +1,6 @@
+from commonfunctions import *
+
+img = rgb2gray(io.imread('dataset/scanned_sheet.jpg'))
+img_filtered = gaussian(img, sigma=1);
+
+show_images([img, img_filtered], ["original image", "filtered image"])
