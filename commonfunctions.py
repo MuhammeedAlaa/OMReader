8b486b08ebc6499c80dcd1f83fefb6b903e8ce45
hossamalaa69 <hossamalaa69.ha@gmail.com>
2020-12-28 01:47:12 +0200

    add projection correction

--- commonfunctions.py
+++ commonfunctions.py
@@ -1,5 +1,3 @@
-
-
 import skimage.io as io
 import matplotlib.pyplot as plt
 import numpy as np
@@ -21,11 +19,77 @@ from skimage.feature import canny
 from skimage.transform import hough_line, hough_line_peaks
 from skimage.transform import rotate
 
+import cv2
+import imutils
+from skimage import exposure
+from skimage.morphology import binary_closing
+
+
+def order_points(pts):
+    rect = np.zeros((4, 2), dtype="float32")
+
+    s = pts.sum(axis=1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+
+    diff = np.diff(pts, axis=1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
+    return rect
+
+
+def four_point_transform(image, pts):
+    rect = order_points(pts)
+
+    (tl, tr, br, bl) = rect
+
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype="float32")
+
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+    return warped
+    
+def deskew_projection(img):  
+    img_gray = img
+    img = cv2.Canny(img, 100, 200)
+    se = np.ones((50, 50))
+    #img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, se).astype(np.uint8)
+    img = cv2.dilate(img, se,iterations = 1).astype(np.uint8)
+    #io.imshow(img)
+    countours, hierarcy = cv2.findContours(img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    countours_sorted = sorted(countours, key=cv2.contourArea, reverse=True)
+    contour = countours_sorted[0]
+    
+    perim = cv2.arcLength(contour, True)
+    epsilon = 0.02*perim
+    approxCorners = cv2.approxPolyDP(contour, epsilon, True)
+    approxCornersNumber = len(approxCorners)
+    
+    #plt.imshow(img_orig)
+    #for p in approxCorners:
+    #    plt.plot(p[0][0], p[0][1], 'bo')
+    #plt.show()
+    
+    pt = np.array(approxCorners).reshape(approxCornersNumber, 2)
+    img_fixed = four_point_transform(img_gray, pt)
+    return img_fixed
+
+
 
 
 # Show the figures / plots inside the notebook
-
-
 def show_images(images, titles=None):
     # This function is used to show image(s) with titles by sending an array of images and an array of associated titles.
     # images[0] will be drawn with the title titles[0] if exists
