6471bcfa385417ddef85fe90c43fb87a9047254a
MuhammeedAlaa <soupermado@gmail.com>
2021-01-04 22:30:16 +0200

    classifcation of 1/2 1/4 1/8 /16 /132 and chords

--- stemRemoval.py
+++ stemRemoval.py
@@ -6,54 +6,58 @@ from itertools import groupby
 
 
 def stemRemoval(img, staffLineSpacing):
-  outputImg = np.copy(img)
-  height, width = outputImg.shape
-  verticalProjection= (np.sum((255 - outputImg)/255, axis=0)).astype('uint64')
-  maxVertical = 3 * staffLineSpacing
-  mask = np.where(verticalProjection <= maxVertical, 0, 1)
-  runlengths, startpositions, values = rle(mask)
-  stemsWidths = runlengths[np.nonzero(values)[0]]
-  stemsPositions = startpositions[np.nonzero(values)[0]]
+    outputImg = np.copy(img)
+    height, width = outputImg.shape
+    verticalProjection = (
+        np.sum((255 - outputImg)/255, axis=0)).astype('uint64')
+    maxVertical = 3 * staffLineSpacing
+    mask = np.where(verticalProjection <= maxVertical, 0, 1)
+    runlengths, startpositions, values = rle(mask)
+    stemsWidths = runlengths[np.nonzero(values)[0]]
+    stemsPositions = startpositions[np.nonzero(values)[0]]
 
-  # assume that the stem is almost vertical (bounding box of the stem width = 5)
-  maxStemSkew = 5
-  maxStemWidth = np.max(stemsWidths, initial=0)
-  threshold = maxStemSkew /2 
+    # assume that the stem is almost vertical (bounding box of the stem width = 5)
+    maxStemSkew = 5
+    maxStemWidth = np.max(stemsWidths, initial=0)
+    threshold = maxStemSkew / 2
 
-  for stem in stemsPositions:
-    for y in range(0, height, 1):
-      if outputImg[y, stem] != 0:
-        for j in range(1, round(threshold/2)):
-          if stem + j < width and outputImg[y, stem + j] == 0:
-            stem = stem + j
-            break
-          if stem - j > -1 and outputImg[y, stem - j] == 0:
-            stem = stem - j
-            break
-      horizontalThresholdResult = testHorizontalThreshold(outputImg, stem, y, maxStemWidth)
-      if(horizontalThresholdResult[0]):
-        rr, cc = line(y, horizontalThresholdResult[1], y, horizontalThresholdResult[2])
-        outputImg[rr, cc] = 255
-  return outputImg, list(zip(stemsPositions, stemsWidths))
-
+    for stem in stemsPositions:
+        for y in range(0, height, 1):
+            if outputImg[y, stem] != 0:
+                for j in range(1, round(threshold/2)):
+                    if stem + j < width and outputImg[y, stem + j] == 0:
+                        stem = stem + j
+                        break
+                    if stem - j > -1 and outputImg[y, stem - j] == 0:
+                        stem = stem - j
+                        break
+            horizontalThresholdResult = testHorizontalThreshold(
+                outputImg, stem, y, maxStemWidth)
+            if(horizontalThresholdResult[0]):
+                rr, cc = line(
+                    y, horizontalThresholdResult[1], y, horizontalThresholdResult[2])
+                outputImg[rr, cc] = 255
+    return outputImg, list(zip(stemsPositions, stemsWidths))
 
 
 # returns the run lengths of the ones in the input array
 def runs_of_ones_list(bits):
-  return [sum(g) for b, g in groupby(bits) if b]
+    return [sum(g) for b, g in groupby(bits) if b]
 
 # returns: tuple (runlengths, startpositions, values)
-def rle(bits):
-  n = len(bits)
-  if n == 0: 
-    return (None, None, None)
-  else:
-    y = np.array(bits[1:] != bits[:-1])     # pairwise unequal (string safe)
-    i = np.append(np.where(y), n - 1)   # must include last element posi
-    lengths = np.diff(np.append(-1, i))       # run lengths
-    positions = np.cumsum(np.append(0, lengths))[:-1] # positions
-    return(lengths, positions, bits[i])
 
+
+def rle(bits):
+    n = len(bits)
+    if n == 0:
+        return (None, None, None)
+    else:
+        # pairwise unequal (string safe)
+        y = np.array(bits[1:] != bits[:-1])
+        i = np.append(np.where(y), n - 1)   # must include last element posi
+        lengths = np.diff(np.append(-1, i))       # run lengths
+        positions = np.cumsum(np.append(0, lengths))[:-1]  # positions
+        return(lengths, positions, bits[i])
 
 
 def testHorizontalThreshold(img, x, y, threshold):
